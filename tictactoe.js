928549bf87a48f9a1756b6370cb72740b8e6bc25
Cesar Juliano <pochettejuliano@gmail.com>
2015-05-05 15:16:37 -0400

    Aesthetics and code refactor

--- tictactoe.js
+++ tictactoe.js
@@ -8,12 +8,21 @@ var cellCount = 0;
 var clickCount = 0;
 
 
+function onTileOver () {
+  $( this ).css( "background-color", "darkolivegreen" );
+}
+
+function onTileOut () {
+  $( this ).css( "background-color", "" );
+}
+
 function onTileClick () {
   var xSum = 0, oSum = 0;
+  var gameEnd = false;
 
   // display the symbol, disable the click, and update the click count
   $( this ).text( clickCount == 0 || clickCount % 2 == 0 ? SYMBOL_X : SYMBOL_O );
-  $( this ).off( "click" );
+  $( this ).off( "click" ).off( "mouseenter" ).off( "mouseleave" );
   clickCount++;
 
   // accumulate the binary values
@@ -28,12 +37,20 @@ function onTileClick () {
   if ( checkForWin( xSum ) ) {
     // check if win for X
     $( "body" ).append( "<h2>X Wins! Reload the page to play again.</h2" );
+    gameEnd = true;
   } else if ( checkForWin( oSum ) ) {
     // check if win for O
     $( "body" ).append( "<h2>O Wins! Reload the page to play again.</h2" );
+    gameEnd = true;
   } else if ( clickCount == cellCount ) {
     // check if draw
     $( "body" ).append( "<h2>It's a tie! Reload the page to play again.</h2" );
+    gameEnd = true;
+  }
+
+  // clean up: remove listeners
+  if ( gameEnd ) {
+    $( "td" ).off( "click" ).off( "mouseenter" ).off( "mouseleave" );
   }
 }
 
@@ -70,5 +87,5 @@ function createBoard () {
 // STARTING POINT //
 $( document ).ready( function () {
   createBoard();
-  $( "td" ).click( onTileClick );
+  $( "td" ).click( onTileClick ).mouseenter( onTileOver ).mouseleave( onTileOut );
 } );
